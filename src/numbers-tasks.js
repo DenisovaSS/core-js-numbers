e2ff5e32d486e4fa6b6accc3bc535ebd70beb1eb
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-11-13 15:23:36 +0200

    feat: add 56 decisions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -636,8 +636,8 @@ function getRandomInteger(min, max) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
@@ -653,8 +653,15 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  const posNum = Math.abs(number);
+  let count = 0;
+  for (let i = 1; i <= posNum; i += 1) {
+    if (i % 2) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
